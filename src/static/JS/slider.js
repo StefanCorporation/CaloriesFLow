c9084bea8d4a80a1ed30687b66a79ac813798ebc
Stefan Ashugyan <abrams1234518@gmail.com>
2025-06-02 21:01:08 +0400

    Fix slider

--- src/static/JS/slider.js
+++ src/static/JS/slider.js
@@ -1,13 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var slider = document.getElementById('customRange3');
-    var output = document.getElementById('value');
+    var sliders = document.querySelectorAll('.slider-range')
 
-    output.innerHTML = slider.value;
+    sliders.forEach(function(slider) {
+        var outputId = 'value' + slider.id.replace('customRange', '')
+        var output = document.getElementById(outputId)
 
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-    }
+        output.innerHTML = slider.value
+
+        slider.addEventListener('input', function() {
+            output.innerHTML = this.value
+        })
+
+    })
 })
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var slider = document.getElementById('customRange3');
+//     var output = document.getElementById('value');
+
+//     output.innerHTML = slider.value;
+
+//     slider.oninput = function() {
+//         output.innerHTML = this.value;
+//     }
+// })
 
 
 
