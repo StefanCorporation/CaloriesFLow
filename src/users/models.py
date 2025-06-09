d69b454810f562473a6ce1e12b122ed13ccedc5c
Stefan Ashugyan <abrams1234518@gmail.com>
2025-06-09 22:07:17 +0400

    Crate user model and upgrade profile and registration pages

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    images = models.ImageField(upload_to='users_avatars', blank=True, null=True, verbose_name='Avatar')
+
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
